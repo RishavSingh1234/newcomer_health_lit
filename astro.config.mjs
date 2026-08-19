640c333b595df43e428837c7d4af612a003d426b
rishavsingh1234 <rishav703@gmail.com>
2026-08-18 20:29:16 -0700

    Configure custom domain and canonical SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://newcomer-health-lit.vercel.app',
+  site: 'https://www.newcomerhealthliteracy.ca',
   integrations: [sitemap()]
 });
